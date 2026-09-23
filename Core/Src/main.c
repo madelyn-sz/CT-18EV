036a075ca9ff6df188d998f267491792f9ac73e3
sufferiing <austinjs1213@gmail.com>
2026-09-22 20:07:25 -0400

    Set up C++ & Eigen

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -31,6 +31,8 @@
 #include "launch_control.h"
 #include "regen.h"
 #include "soc_kf.h"
+#include "soc_kf_eigen.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -219,7 +221,6 @@ static int32_t torque_lut(float tps) {
   */
 int main(void)
 {
-
   /* USER CODE BEGIN 1 */
 	for (uint32_t i = 0; i < sizeof(RxData); i++) {
 		RxData[i] = 0;
---
+++ Core/Src/soc_kf_eigen.cpp
@@ -0,0 +1,14 @@
+/*
+ * soc_kf_eigen.cpp
+ *
+ *  Created on: Sep 22, 2026
+ *      Author: Madelyn
+ */
+
+
+#include "soc_kf_eigen.h"
+#include "Eigen/Core"
+
+void will_eigen_compile() {
+	Eigen::Matrix<double, 5, 5> x;
+}
